f46eada2b9cdb904712103b9d0a484418a8ed817
Bitmads <1665855-bitmads@users.noreply.gitlab.com>
2025-07-22 22:45:57 -0700

    Add module-resolver plugin for shorthand imports

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module:react-native-dotenv']
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@state': './src/state',
+          '@app-types': './types',
+        },
+      },
+    ],
   ]
 };
