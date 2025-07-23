33426eb5ec9f1fd045e019ece495c6fe9ec26743
Bitmads <1665855-bitmads@users.noreply.gitlab.com>
2025-07-23 00:29:07 -0700

    Add constants and typings

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const LOG_API_REQUESTS: boolean = true;
+export const LOG_API_RESPONSES: boolean = true;
+export const LOG_API_ERRORS: boolean = true;
+export const DEFAULT_COLOR_SCHEME = 'light'
