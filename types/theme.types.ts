4c28aeb93174362827dc0076a40313200f15d9f7
pitt <git@btimads.com>
2025-08-01 04:01:26 -0700

    Update Theme engine typing

--- types/theme.types.ts
+++ types/theme.types.ts
@@ -18,16 +18,32 @@ export type SpacerTypes = MarginTypes | PaddingTypes | HeadingTypes | GapTypes;
 export type SpacerStyles = { [key in SpacerTypes] : any};
 
 
-export type ContextualClasses = 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info';
+export type ContextualClasses = 'primary' | 'secondary' | 'accent' | 'success' | 'danger' | 'warning' | 'info';
 export type ContextualColors = Record<ContextualClasses, string>;
 
-export type ThemeType = { colors: Colors, styles: HelperStyles & CoreComponentStyles };
+export type ThemeType = { colors: Colors, styles: HelperStyles & CoreComponentStyles, spacing: { [key: number]: number } };
 export type Colors = GeneralColors & ThemeColors & ContextualColors;
-export type HelperStyles = { background: any; text: any ; row: any; spaceBetween: any; col: {auto: any; grow: any}} & SpacerStyles;
 export type StyleTypes = ViewStyle & TextStyle & ImageStyle;
 
+export type ColumnSize = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
-export type CoreComponents = 'Text' | 'Bg' | 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' | 'Link' | 'TextInput' | 'Button' | 'ButtonDefault' | 'ButtonDefaultText' | 'ButtonPrimary' | 'ButtonPrimaryText' | 'Card' | 'CardDefault' | 'Label' | 'LabelText' | 'Tag' | 'TagText';
+export type ColStyles = {
+  auto: StyleTypes;
+  grow: StyleTypes;
+} & {
+  [key in `col${ColumnSize}`]: StyleTypes;
+};
+
+export type HelperStyles = {
+  background: any; text: any ;
+  row: any;
+  col: ColStyles;
+  justifyContent: {start: StyleTypes; center: StyleTypes; spaceBetween: StyleTypes; spaceAround: StyleTypes; spaceEvenly: StyleTypes; }
+  alignItems: {start: StyleTypes; end: StyleTypes; center: StyleTypes; stretch: StyleTypes, baseline: StyleTypes }
+} & SpacerStyles;
+
+
+export type CoreComponents = 'Text' | 'Bg' | 'H1' | 'H2' | 'H3' | 'H4' | 'H5' | 'H6' | 'Lead' | 'Link' | 'TextInput' | 'Button' | 'ButtonDefault' | 'ButtonDefaultText' | 'ButtonPrimary' | 'ButtonPrimaryText' | 'Card' | 'CardText' | 'CardDefault' | 'Label' | 'LabelText' | 'Tag' | 'TagText' | 'Code';
 export type CoreComponentStyles = Record<CoreComponents, StyleTypes>;
 export type ColorSchemes = 'light' | 'dark';
 export type ColorSchemeSettings = 'light' | 'dark' | 'system';
