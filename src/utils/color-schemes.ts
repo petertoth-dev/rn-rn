662878245734871abb6631134b8f23932b3242ae
pitt <git@btimads.com>
2025-08-01 14:27:53 -0700

    Handles a race-condition issue

--- src/utils/color-schemes.ts
+++ src/utils/color-schemes.ts
@@ -1,23 +1,20 @@
 import { useEffect } from 'react';
-import { Appearance, ColorSchemeName } from 'react-native';
+import { Appearance } from 'react-native';
 import useStore from '@state/store.ts';
 import { DEFAULT_COLOR_SCHEME } from '@src/constants.ts';
 
 export const useColorSchemes = () => {
   const setAppState = useStore((state) => state.app.set);
 
-  useEffect(() => {
-    const currentScheme = Appearance.getColorScheme();
-    setAppState(state => {state.app.system.colorScheme = currentScheme || DEFAULT_COLOR_SCHEME});
+  // Initialize
+  const currentScheme = Appearance.getColorScheme();
+  setAppState(state => { state.app.system.colorScheme = currentScheme || DEFAULT_COLOR_SCHEME });
 
-    const subscription = Appearance.addChangeListener(({ colorScheme }: { colorScheme: ColorSchemeName }) => {
-      setAppState(state => {state.app.system.colorScheme = colorScheme || DEFAULT_COLOR_SCHEME});
+  useEffect(() => {
+    const subscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setAppState(state => { state.app.system.colorScheme = colorScheme || DEFAULT_COLOR_SCHEME });
     });
 
-    // Cleanup
-    return () => {
-      subscription.remove();
-    };
+    return () => subscription.remove();
   }, []);
-
-}
+};
