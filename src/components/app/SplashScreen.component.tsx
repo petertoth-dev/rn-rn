733f3778ed952c04925df094263468cc76647d6a
pitt <git@btimads.com>
2025-08-04 02:52:30 -0700

    Update styling

--- src/components/app/SplashScreen.component.tsx
+++ src/components/app/SplashScreen.component.tsx
@@ -1,10 +1,13 @@
 import { ActivityIndicator, Text, View } from 'react-native';
+import { useTheme } from '@src/themes/theme.context.tsx';
 
 const SplashScreen = () => {
+  const { Theme } = useTheme();
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', alignSelf: 'center'}}>
-      <Text style={{color: 'white', marginBottom: 13}}>Loading...</Text>
-      <ActivityIndicator size="large"/>
+    <View style={[Theme.styles.container.flex, Theme.styles.container.center]}>
+      <Text style={[Theme.styles.Text, Theme.styles.mb4]}>Loading...</Text>
+      <ActivityIndicator size="large" color={Theme.colors.primary}/>
     </View>
   );
 };
